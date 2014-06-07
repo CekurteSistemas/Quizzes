50165fd0c3788c6f82a9daab050e8c44283b9312
João Paulo Cercal <sistemas@cekurte.com>
2014-06-07 14:54:47 -0300

    Custom ZCPE CKeditor Toolbar

--- src/Cekurte/Custom/GeneratorBundle/Resources/public/ckeditor/config.js
+++ src/Cekurte/Custom/GeneratorBundle/Resources/public/ckeditor/config.js
@@ -7,10 +7,20 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-	//config.extraPlugins = 'youtube';
+    config.extraPlugins = 'syntaxhighlight';
 
 	config.removePlugins = 'forms,flash,smiley,specialchar';
 	config.filebrowserBrowseUrl =  'http://www.3minovacao.com.br/vendor/kfm/'
     config.entities         = false;
     config.entities_greek   = false;
+
+    config.toolbar = 'ZCPEToolbar';
+
+    config.toolbar_ZCPEToolbar =
+    [
+        { name: 'document', items : [ 'NewPage','Preview','Bold','Italic','Strike','-','RemoveFormat' ] },
+        { name: 'insert', items : [ 'Syntaxhighlight' ] },
+        { name: 'basicstyles', items : [ 'Bold','Italic','Strike','-','RemoveFormat' ] },
+        { name: 'tools', items : [ 'Maximize' ] }
+    ];
 };
