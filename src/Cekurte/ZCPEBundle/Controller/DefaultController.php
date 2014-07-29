e0276dfb15f434fa1a40091015d653081b5dd232
João Paulo Cercal <sistemas@cekurte.com>
2014-07-29 18:20:20 -0300

    Parser of questions.

--- src/Cekurte/ZCPEBundle/Controller/DefaultController.php
+++ src/Cekurte/ZCPEBundle/Controller/DefaultController.php
@@ -2,12 +2,14 @@
 
 namespace Cekurte\ZCPEBundle\Controller;
 
+use Cekurte\ZCPEBundle\Entity\Parser;
 use Cekurte\ZCPEBundle\Entity\Question;
 use Cekurte\ZCPEBundle\Form\Type\QuestionAnonymousFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\DomCrawler\Crawler;
 
 class DefaultController extends Controller
 {
@@ -24,6 +26,76 @@ class DefaultController extends Controller
             'form'              => $form->createView(),
             'subject_template'  => sprintf('%s: ', $this->container->getParameter('cekurte_zcpe_google_group_subject')),
         );
+    }
+
+    /**
+     * @Route("/parser")
+     * @Method("GET")
+     * @Template()
+     */
+    public function parserAction()
+    {
+        set_time_limit(0);
+
+        $rootDir = $this->get('kernel')->getRootDir();
+
+        $content = file_get_contents(realpath($rootDir . '/../docs/google-groups-posts.bkp.html'));
+
+        $crawler = new Crawler($content);
+
+        $subjectFilterPrefix = 'pergunta';
+
+        $em = $this->get('doctrine')->getManager();
+
+        $crawler->filter('body > table > tbody > tr > td > div > div > div:first-child')->each(function (Crawler $node, $i) use (&$subjectFilterPrefix, $em) {
+
+            $subject    = $node->filter('a')->first();
+            $author     = $node->filter('div:first-child > div')->attr('data-name');
+
+            $time  = $node->filter('div')->last()->children()->attr('title');
+
+            setlocale(LC_ALL, NULL);
+            setlocale(LC_ALL, 'pt_BR');
+
+            if (substr(strtolower(utf8_decode($subject->text())), 0, strlen($subjectFilterPrefix)) == $subjectFilterPrefix) {
+
+                $timeParts = explode(',', utf8_decode($time));
+
+                $timeParsed = strptime(
+                    end($timeParts),
+                    '%d de %B de %Y %Hh%Mmin%Ss'
+                );
+
+                $createdAt = new \DateTime(date('Y-m-d h:i:s', mktime(
+                    $timeParsed['tm_hour'],
+                    $timeParsed['tm_min'],
+                    $timeParsed['tm_sec'],
+                    1,
+                    $timeParsed['tm_yday'] + 1,
+                    $timeParsed['tm_year'] + 1900
+                )));
+
+                $entity = $em->getRepository('CekurteZCPEBundle:Parser')->findOneBy(array(
+                    'subject' => utf8_decode($subject->text())
+                ));
+
+                if (!$entity instanceof Parser) {
+
+                    $parser = new Parser();
+                    $parser
+                        ->setSubject(utf8_decode($subject->text()))
+                        ->setUrl($subject->attr('href'))
+                        ->setAuthor(utf8_decode($author))
+                        ->setCreatedAt($createdAt)
+                    ;
+
+                    $em->persist($parser);
+                    $em->flush();
+                }
+            }
+        });
+
+        return array();
     }
 
     /**
---
+++ src/Cekurte/ZCPEBundle/Entity/Parser.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace Cekurte\ZCPEBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Parser
+ *
+ * @ORM\Table(name="parser_question_google_groups")
+ * @ORM\Entity
+ */
+class Parser
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="subject", type="string", length=255)
+     */
+    private $subject;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="url", type="string", length=255)
+     */
+    private $url;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="author", type="string", length=255, nullable=true)
+     */
+    private $author;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $createdAt;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set subject
+     *
+     * @param string $subject
+     * @return Parser
+     */
+    public function setSubject($subject)
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    /**
+     * Get subject
+     *
+     * @return string 
+     */
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     * @return Parser
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string 
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Set author
+     *
+     * @param string $author
+     * @return Parser
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return string 
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return Parser
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+}
